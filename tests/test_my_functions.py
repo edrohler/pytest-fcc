57709cea9fa8974c810403728363808e4189b06c
Eric D. Rohler <development@ericrohler.com>
2023-12-28 21:20:09 -0500

    Add additional basic test

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -6,6 +6,10 @@ import source.my_functions as my_functions
 def test_add():
     assert my_functions.add(1, 2) == 3
     
+def test_add_string():
+    result = my_functions.add('Hello', 'World')
+    assert result == 'HelloWorld'
+    
 def test_divide():
     assert my_functions.divide(10, 2) == 5
     with pytest.raises(ZeroDivisionError):
