0657b70406971884437b488ca25228ffb6992ef8
Eric D. Rohler <erohler@wearemoore.com>
2023-12-29 09:25:03 -0500

    Add marking examples

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 
 import source.my_functions as my_functions
 
@@ -14,3 +15,17 @@ def test_divide():
     assert my_functions.divide(10, 2) == 5
     with pytest.raises(ZeroDivisionError):
         my_functions.divide(5, 0)
+        
+@pytest.mark.slow
+def test_very_slow():
+    time.sleep(5)
+    result = my_functions.divide(10,5)
+    assert result == 2
+    
+@pytest.mark.skip(reason='Not implemented yet.')
+def test_multiply():
+    assert my_functions.multiply(2, 3) == 6
+    
+@pytest.mark.xfail(reason='Not implemented yet.')
+def test_subtract():
+    assert my_functions.subtract(5, 2) == 3
