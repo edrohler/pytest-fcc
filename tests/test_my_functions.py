7321b0660b0e8852962f143ea2dc5f5ceaff8adb
Eric D. Rohler <development@ericrohler.com>
2023-12-28 21:12:05 -0500

    Add first tests

---
+++ tests/test_my_functions.py
@@ -0,0 +1,12 @@
+import pytest
+
+import source.my_functions as my_functions
+
+
+def test_add():
+    assert my_functions.add(1, 2) == 3
+    
+def test_divide():
+    assert my_functions.divide(10, 2) == 5
+    with pytest.raises(ZeroDivisionError):
+        my_functions.divide(5, 0)
